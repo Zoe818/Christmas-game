0c3b5db4169da40b3269e873af71ac31a9d740a6
DarkKris <long4664030@163.com>
2019-12-20 10:15:32 +0800

    feat: upload_key

---
+++ js/count.js
@@ -0,0 +1,21 @@
+function count(type) {
+    switch(type) {
+        case 'enter': // 进入游戏
+            break;
+        case 'play': // 玩游戏
+            break;
+        case 'played': // 被游玩
+            break;
+        case 'screen': // 截图
+            break;
+        case 'save': // 保存头像
+            break;
+        case 'level1': // 第一关
+            break;
+        case 'level2': // 第二关
+            break;
+        case 'level3': // 第三关
+            break;
+    }
+    request('')
+}
