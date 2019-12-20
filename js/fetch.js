202e0e0a632267e36d7ba01f35e690f47d54b77e
DarkKris <long4664030@163.com>
2019-12-20 11:40:22 +0800

    feat: 埋点 以及 key

--- js/fetch.js
+++ js/fetch.js
@@ -52,3 +52,31 @@ async function request(url = '', method = 'GET', data = {} ){
         throw new Error(error)
     }
 }
+
+function count(type) {
+    // switch(type) {
+    //     case 'enter': // 进入游戏
+    //         break;
+    //     case 'play': // 玩游戏
+    //         break;
+    //     case 'played': // 被游玩
+    //         break;
+    //     case 'screen': // 截图
+    //         break;
+    //     case 'save': // 保存头像
+    //         break;
+    //     case 'level1': // 第一关
+    //         break;
+    //     case 'level2': // 第二关
+    //         break;
+    //     case 'level3': // 第三关
+    //         break;
+    // }
+    const key = 'wo xian sui bian xie';
+    for(let i=0;i<parseInt(type);i++) {
+        request('/data', 'GET', {
+            data: 'level'+type,
+            check: md5('level'+type + key)
+        })
+    }
+}
