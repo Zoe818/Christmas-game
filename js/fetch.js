3bca45addf30a7c88e7c51831e58af52722a40e8
Zoe <916168470@qq.com>
2019-12-24 15:47:26 +0800

    debug

--- js/fetch.js
+++ js/fetch.js
@@ -2,8 +2,8 @@
   请求函数
  */
 
-// let baseUrl = 'https://hackday.itoken.team/api/christmas2019/';
-let baseUrl = 'http://123.207.188.192:8080/';
+let baseUrl = 'https://hackday.itoken.team/api/christmas2019/';
+//let baseUrl = 'http://123.207.188.192:8080/';
 async function request(url = '', method = 'GET', data = {} ){
     // method大写
     method = String(method).toUpperCase();
